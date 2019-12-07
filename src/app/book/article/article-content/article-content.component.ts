44e037ade1b58592ef5098a364cb9004c2cab269
Alex Oleynichenko <oleynichenkos@gmail.com>
2019-12-07 15:09:05 +0200

    footnotes added to article template

--- src/app/book/article/article-content/article-content.component.ts
+++ src/app/book/article/article-content/article-content.component.ts
@@ -1,33 +1,57 @@
-import {Component, Input, OnInit} from '@angular/core';
-
-interface ChunkData {
-  id: string;
-  langId: string;
-  authorId: string;
-}
+import {
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  OnChanges, OnDestroy,
+  OnInit,
+  QueryList, Renderer2,
+  ViewChild,
+  ViewChildren
+} from '@angular/core';
+import {Article, Chunk} from '../../../app.model';
+import {MatSnackBar} from '@angular/material';
+import {first} from 'rxjs/operators';
 
 @Component({
   selector: 'app-article-content',
   templateUrl: './article-content.component.html',
   styleUrls: ['./article-content.component.scss']
 })
-export class ArticleContentComponent implements OnInit {
-  @Input() set articles(value: any[]) {
-    if (value.length > 0) {
-      this.content = [];
-      const mainArticle = value[0];
-      this.title = mainArticle.title;
+export class ArticleContentComponent implements OnInit, OnChanges, AfterViewChecked, OnDestroy {
+  @Input() set articleContent(data: Article[]) {
+    if (data && data.length) {
+      this.footnotes = this._getAllFootnotes(data);
 
-      this.content = mainArticle.chunks.map((c) => {
-        c.langId = value[0].langId;
-        const chunkData = [c];
+      const firstArticle: Article = data[0];
 
-        for (let i = 1; i < value.length; i++) {
-          const chunk = this._getChunkById(value[i].chunks, c.chunkId);
-          if (chunk) {
-            chunk.langId = value[i].langId;
-            chunkData.push(chunk);
-          }
+      this.content = firstArticle.chunks.map((c) => {
+        // создаем массив chunkData для хранения всех чанков с одинаковым id
+
+        const chunkData = [{
+          chunkId: c.chunkId,
+          content: (firstArticle.footnotes && firstArticle.footnotes.length)
+            ? this._pasteFootnotes(c.content, firstArticle.langId)
+            : c.content,
+          langId: firstArticle.langId,
+        }];
+
+        for (let i = 1; i < data.length; i++) {
+          const article = data[i];
+          const chunk = this._getChunkById(article.chunks, c.chunkId);
+
+          const chunkContent = chunk
+            ? (article.footnotes && article.footnotes.length)
+              ? this._pasteFootnotes(chunk.content, article.langId)
+              : chunk.content
+            : '';
+
+          chunkData.push({
+            chunkId: chunk.chunkId,
+            content: chunkContent,
+            langId: article.langId
+          });
         }
 
         return chunkData;
@@ -35,15 +59,108 @@ export class ArticleContentComponent implements OnInit {
     }
   }
 
-  title: string;
-  content: ChunkData[][];
+  content: Chunk[][];
+  private footnotes: {};
+  private footnotesElems: NodeList;
+  private contentChanged: boolean;
 
-  constructor() { }
+  constructor(private hostElementRef: ElementRef,
+              private renderer: Renderer2,
+              private snackBar: MatSnackBar) { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  ngOnChanges(changes) {
+    if (changes.articleContent && changes.articleContent.currentValue.length) {
+      this.contentChanged = true;
+      this.snackBar.dismiss();
+    }
   }
 
-  _getChunkById(chunks, id) {
+  ngAfterViewChecked() {
+    if (this.contentChanged) {
+      this.footnotesElems = this.getFootnotesElems();
+      this.setListenersToFootnotes(this.footnotesElems);
+      this.contentChanged = false;
+    }
+  }
+
+  private getFootnotesElems() {
+    return this.hostElementRef.nativeElement
+      .querySelectorAll('.loaded-content__footnote');
+  }
+
+  private setListenersToFootnotes(elems: NodeList) {
+    elems.forEach((f) => {
+      if (f.isConnected) {
+        this.renderer.listen(
+          f,
+          'click',
+          this.onFootnoteClick.bind(this)
+        );
+      }
+    });
+  }
+
+  private onFootnoteClick(event) {
+    if (!event.target.classList.contains('loaded-content__footnote--chosen')) {
+      const data = event.target.dataset;
+
+      const footnoteText = this.footnotes[data.footnote]
+        || 'Sorry, this footnote is not set yet...';
+
+      const direction = (data.lang === 'he') ? 'rtl' : 'ltr';
+
+      const snackBarRef = this.snackBar.open(footnoteText, 'X', {direction});
+
+      this.renderer.addClass(
+        event.target,
+        'loaded-content__footnote--chosen'
+      );
+
+      snackBarRef.afterDismissed()
+        .pipe(first())
+        .subscribe(info => {
+          this.renderer.removeClass(
+            event.target,
+            'loaded-content__footnote--chosen'
+          );
+      });
+    }
+  }
+
+  private _getChunkById(chunks, id) {
     return chunks.find((i) => i.chunkId === id);
+  }
+
+  private _pasteFootnotes(content: string, lang: string): string {
+    const regExp = /\[\d{1,}\]/g; // поиск [число]
+
+    return content.replace(regExp, (match) => {
+      const footnoteId = match.substring(1, match.length - 1);
+      const footnoteName = this._getFootnoteName(footnoteId, lang);
+
+      return `<a class="loaded-content__footnote" data-footnote="${footnoteName}" data-lang="${lang}"></a>`;
+    });
+  }
+
+  private _getFootnoteName(id, lang) {
+    return `${lang}_${id}`;
+  }
+
+  private _getAllFootnotes(data: Article[]) {
+    return data.reduce((res, a) => {
+      if (a.footnotes) {
+        a.footnotes.forEach((f) => {
+          res[this._getFootnoteName(f.id, a.langId)] = f.text;
+        });
+      }
+
+      return res;
+    }, {});
+  }
+
+  ngOnDestroy() {
+    this.snackBar.dismiss();
   }
 }
